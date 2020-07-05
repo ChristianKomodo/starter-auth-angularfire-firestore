6aa937563bbe38480bd8a21052e5d58b31e3540f
ChristianKomodo <chris@sectorfive.com>
2020-07-05 19:35:52 -0400

    login functionality moved to /login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(public auth: AuthService) {}
-}
+export class AppComponent {}
